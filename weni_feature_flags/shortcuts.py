634c73856be2655f76b909146a456b92944f0c02
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-10-10 11:28:37 -0300

    Add new method to check feature activity by user email and project UUID; rename existing method for clarity in feature flag evaluation.

--- weni_feature_flags/shortcuts.py
+++ weni_feature_flags/shortcuts.py
@@ -1,7 +1,8 @@
+from uuid import UUID
 from weni_feature_flags.services import FeatureFlagsService
 
 
-def is_feature_active(key: str, attributes: dict) -> bool:
+def is_feature_active_for_attributes(key: str, attributes: dict) -> bool:
     """
     Check if a feature is active by its key and attributes.
     """
@@ -10,4 +11,18 @@ def is_feature_active(key: str, attributes: dict) -> bool:
     return features_service.evaluate_feature_flag_by_attributes(
         key=key,
         attributes=attributes,
+    )
+
+
+def is_feature_active(key: str, user_email: str, project_uuid: UUID) -> bool:
+    """
+    Check if a feature is active by user email and project uuid.
+    """
+
+    return is_feature_active_for_attributes(
+        key,
+        {
+            "userEmail": user_email,
+            "projectUUID": project_uuid,
+        },
     )
