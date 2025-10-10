dfef504530896317c1e06a6ef556bf4e4d3ec63a
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-10-10 15:30:48 -0300

    Enhance unit tests for feature flag evaluation by adding tests for is_feature_active and is_feature_active_for_attributes functions; utilize mock service for improved test reliability.

--- weni_feature_flags/tests/test_shortcuts.py
+++ weni_feature_flags/tests/test_shortcuts.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from unittest.mock import Mock, patch
+import uuid
 
 from weni_feature_flags.services import FeatureFlagsService
 from weni_feature_flags.shortcuts import (
@@ -7,16 +8,44 @@ from weni_feature_flags.shortcuts import (
     is_feature_active_for_attributes,
 )
 
+mock_service = Mock(spec=FeatureFlagsService)
 
+
+@patch("weni_feature_flags.shortcuts.FeatureFlagsService", return_value=mock_service)
 class TestShortcuts(TestCase):
     def setUp(self):
         self.mock_service = Mock(spec=FeatureFlagsService)
 
-    @patch("weni_feature_flags.shortcuts.FeatureFlagsService")
     def test_is_feature_active_for_attributes_when_the_feature_is_active(
         self, mock_feature_flags_svc
     ):
-        self.mock_service.evaluate_feature_flag_by_attributes.return_value = True
-        mock_feature_flags_svc.return_value = self.mock_service
+        mock_service.evaluate_feature_flag_by_attributes.return_value = True
 
-        self.assertTrue(is_feature_active_for_attributes("test", {"test": "test"}))
+        self.assertTrue(
+            is_feature_active_for_attributes(
+                "test", {"projectUUID": uuid.uuid4(), "userEmail": "test@test.com"}
+            )
+        )
+
+    def test_is_feature_active_for_attributes_when_the_feature_is_not_active(
+        self, mock_feature_flags_svc
+    ):
+        mock_service.evaluate_feature_flag_by_attributes.return_value = False
+
+        self.assertFalse(
+            is_feature_active_for_attributes(
+                "test", {"projectUUID": uuid.uuid4(), "userEmail": "test@test.com"}
+            )
+        )
+
+    def test_is_feature_active_when_the_feature_is_active(self, mock_feature_flags_svc):
+        mock_service.evaluate_feature_flag_by_attributes.return_value = True
+
+        self.assertTrue(is_feature_active("test", "test@test.com", uuid.uuid4()))
+
+    def test_is_feature_active_when_the_feature_is_not_active(
+        self, mock_feature_flags_svc
+    ):
+        mock_service.evaluate_feature_flag_by_attributes.return_value = False
+
+        self.assertFalse(is_feature_active("test", "test@test.com", uuid.uuid4()))
