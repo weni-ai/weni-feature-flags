b8053da36df6e119433e8f7f914e7ebd214e113a
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-11-27 16:58:34 -0300

    Add cooldown mechanism for feature flag updates

--- weni/feature_flags/services.py
+++ weni/feature_flags/services.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from typing import List, Optional
 
 from django.core.cache import cache
@@ -7,10 +8,15 @@ from growthbook import GrowthBook
 from weni.feature_flags.converters import convert_uuids_to_strings
 from weni.feature_flags.integrations.growthbook.clients import GrowthBookClient
 from weni.feature_flags.models import FeatureFlagSnapshot
-from weni.feature_flags.settings import CACHE_KEY_PREFIX, FEATURES_CACHE_TTL
+from weni.feature_flags.settings import CACHE_KEY_PREFIX, FEATURES_CACHE_TTL, FEATURES_UPDATE_COOLDOWN_TTL
 from weni.feature_flags.tasks import update_feature_flags
 
 CACHE_KEY = f"{CACHE_KEY_PREFIX}:features"
+COOLDOWN_CACHE_KEY = f"{CACHE_KEY_PREFIX}:cooldown_is_in_effect"
+COOLDOWN_CACHE_TTL = FEATURES_UPDATE_COOLDOWN_TTL
+
+
+logger = logging.getLogger(__name__)
 
 
 class FeatureFlagsService:
@@ -93,6 +99,12 @@ class FeatureFlagsService:
         """
         Update feature flags.
         """
+        if cache.get(COOLDOWN_CACHE_KEY):
+            # This is a safeguard to prevent the update_features task from being called too often.
+            logger.info("Features update cooldown is in effect. Skipping update.")
+            return
+
+        cache.set(COOLDOWN_CACHE_KEY, True, COOLDOWN_CACHE_TTL)
         features = self.growthbook_client.get_features()
 
         self.save_features_to_db(features)
